aceb22122a3657e9dd14306c4bbd4fb7eb87b2ac
mntroshkin <tr2mikh@gmail.com>
2026-06-26 17:18:36 +0300

    archived old files

--- src/diffalgebra/__init__.py
+++ src/diffalgebra/__init__.py
@@ -1,5 +1,7 @@
-from .constant_ring import ConstantPolyRing, QQ
-from .diff_ring import DifferentialRing, total_derivative, partial_derivative
+from .constant_ring import ConstantPolyRing, QQ, ConstantPolynomial
+from .diff_ring import DifferentialRing, DifferentialPolynomial, total_derivative, partial_derivative
 from .ring_morphism import RingMorphism, DiffRingMorphism
 
-__all__ = [QQ, ConstantPolyRing, DifferentialRing, total_derivative, partial_derivative, RingMorphism, DiffRingMorphism]
+__all__ = ["QQ", "ConstantPolyRing", "DifferentialRing", 
+           "ConstantPolynomial", "DifferentialPolynomial", 
+           "total_derivative", "partial_derivative", "RingMorphism", "DiffRingMorphism"]
